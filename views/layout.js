6a3f0bc2dbf656b5c57ca65dfd672b572c0dd3ab
murtalaliyu <murtalaliyu10@gmail.com>
2022-01-17 00:46:46 -0500

    added admin link on homepage

--- views/layout.js
+++ views/layout.js
@@ -47,6 +47,9 @@ module.exports = ({ content }) => {
               <div class="navbar-item">
                 <div class="navbar-buttons">
                   <div class="navbar-item">
+                    <a href="/signin"><i class="fa fa-lock"></i> Admin</a>
+                  </div>
+                  <div class="navbar-item">
                     <a href="/"><i class="fa fa-star"></i> Products</a>
                   </div>
                   <div class="navbar-item">
